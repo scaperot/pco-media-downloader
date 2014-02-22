92beb845a117aef56070ed32392060d0bfffde8f
CCC <scaperot@vt.edu>
2014-02-22 14:06:35 -0500

    adding a base file for settings.  makes things easier to check in if you keep this separate from index.php.

---
+++ src/com.capcitychurch/settings.php
@@ -0,0 +1,13 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//contact PCO via email to request consumer key/secret for API
+	$settings = array(
+		'key'=>'',
+        	'secret'=>'',
+                'debug'=>true,
+	);
