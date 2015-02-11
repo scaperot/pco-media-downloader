9084a222a804a92a1c3e3cdb731e7ba7d4d833a7
david <scaperot@vt.edu>
2015-02-11 07:40:16 -0500

    made lots of changes today to add most of the helper functions Im using right now to my utility class.  basically I have moved over all the things I need to download songs and media/images into one class.  now I do not need to have a separate index file for all three.  i will check in the changes Ive made then remove references to them afterward just so i have the history maintained.

--- index.php
+++ index.php
@@ -17,6 +17,7 @@
 	require('src/com.rapiddigitalllc/PlanningCenterOnline.php');
 	require('src/com.capcitychurch/settings.php');
         require('src/com.capcitychurch/options.php');
+        require('src/com.capcitychurch/PlanningCenterOnlineUtilities.php');
 
     
     $DC_SITE = 1;
@@ -27,11 +28,13 @@
     $SERVICETYPE = $CAPCITY_SUNDAY_SERVICE; //$CAPCITY_KIDS = 1;
     $SITE = $DC_SITE; //default
     $SITE_STR = 'DC';
-    $DOWNLOAD_DIR = '/var/tmp'; //default
+    //$DOWNLOAD_DIR = '/var/tmp'; //default (built into the utilities class)
     $BLACK_SLIDE  = 'https://planningcenteronline.com/attachments/23267938';
     
 	
 	$pco = new PlanningCenterOnline($settings);
+        $pcoutils = new PlanningCenterOnlineUtilities($pco);
+
 	
 	/**
 	 * BEGIN: Login
@@ -44,7 +47,7 @@
 		die("Login Failed");
 	}
 	
-        $options = getoptions('h::s::p::',array(''));
+        $options = getoptions('h::s::p::t::',array(''));
         //var_dump($options);
 
  foreach (array_keys($options) as $opt) switch ($opt) {
@@ -64,27 +67,40 @@
     break;
 
   case 'p':
-    $DOWNLOAD_DIR=$options['p'];
-    if (!is_writable($DOWNLOAD_DIR)) {
-        echo "Directory $DOWNLOAD_DIR is NOT writeable.  Permission Denied.\n";
+    $pcoutils->setDownloadDir($options['p']);
+    if (!is_writable($options['p'])) {
+        echo "Directory {$options['p']} is NOT writeable.  Permission Denied.\n";
         exit(1);
     }
     break;
+  case 't':
+      if ($options['t'] == "media") {
+          echo "download all media in org.\n";
+          $pcoutils->downloadAllMedia();
+          return;
+      }
+      elseif ($options['t'] == "plan") {
+          echo "download all media by plan.\n";
+      }
+      elseif ($options['t'] == "song") {
+          echo "download all songs in org";
+          $pcoutils->downloadAllSongs();
+          return;
+      }
+      break;
 
   case 'h':
     print_help_message();
     exit(1);
 
-   
+  default:
+    print_help_message();
+    exit(1);
 
 }
-        
-
-
-	#Query for Organization
+  
 	$o = $pco->organization;
-        
-        
+        //By Default attempt to download by plan.
         //Find the most recent Service for site
         $service = $o->service_type_folders[$SITE]->service_types[$SERVICETYPE];
         echo "$SITE_STR Service. ";          
@@ -100,44 +116,8 @@
 
         //$j = json_encode($plan);
         //echo "{$j}\n";
-        
-        //Iterate through all the items in plan and get image attachments...
-        $save_attachments = null;
-        $n = 0;
-        foreach($plan->items as $item){
-            //echo "  Item: $item->title\n";
-            //start saving attachments after the 'Service' title.
-            if ($item->title == "PW Set") {
-                $save_attachments = TRUE;
-            }
-            
-            
-            if ($save_attachments == TRUE) {
-                //iterate through all attachments to find the media files...for now just images.
-                $len = count($item->attachments);
-                //echo "      Saving: at least $len attachments.\n";
-                foreach($item->attachments as $attachment){
-                    //may add other types someday...
-                    if (strpos($attachment->content_type,"image") !== FALSE) {
-                        $n = $n + 1;
-                        echo "      Saving: $DOWNLOAD_DIR/$attachment->filename ($attachment->content_type)\n";
-                        //write to file...
-                        //Other things I should check for: 
-                        //$attachment->downloadable = true, 
-                        $new_file_name = "$DOWNLOAD_DIR/$n. $attachment->filename";
-                        $url = $attachment->url;
-                        //echo "      Saving from URL: $url\n";                        
-                        $r = $pco->getAttachment($url,NULL,OAUTH_HTTP_METHOD_GET,$attachment->content_type);
-                        //download to $download_dir
-                        try {
-                            copy($r['redirect_url'],$new_file_name);
-                        }
-                        catch (Exception $e) {
-                            echo "Failed to Download: {$attachment->filename}, {$e->getMessage}\n";
-                            return; 
-                        }   
-                        
-                    }      
-                }
-            }      
-        }
+        $pcoutils->downloadMediaByPlan($plan);
+
+
+
+
